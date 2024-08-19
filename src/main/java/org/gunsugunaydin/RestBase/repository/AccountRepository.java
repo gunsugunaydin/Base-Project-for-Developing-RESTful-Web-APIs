e56e796c8b069f0f357630e12de2c3bbbbe48892
Günsu Günaydın <110038957+gunsugunaydin@users.noreply.github.com>
2024-08-19 19:19:07 +0300

    Update AccountRepository.java

--- src/main/java/org/gunsugunaydin/RestBase/repository/AccountRepository.java
+++ src/main/java/org/gunsugunaydin/RestBase/repository/AccountRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.gunsugunaydin.RestBase.model.Account;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
 
     Optional<Account> findByEmail(String email);   
