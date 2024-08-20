385a90f95eb2fc9265b16a466494b7e6a90817da
Günsu Günaydın <110038957+gunsugunaydin@users.noreply.github.com>
2024-08-20 17:47:51 +0300

    Update HomeController.java

--- src/main/java/org/gunsugunaydin/RestBase/controller/HomeController.java
+++ src/main/java/org/gunsugunaydin/RestBase/controller/HomeController.java
@@ -4,12 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Home Controller", description = "controller for home -for testing purposes")
 public class HomeController {
 
     @GetMapping("/api/hello")
     public String hello(){
         return "Hello World";
     }
-
-    
 }
