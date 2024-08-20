0ec89ae79cc2f76ec7faf88683bcadb134d533d9
Günsu Günaydın <110038957+gunsugunaydin@users.noreply.github.com>
2024-08-20 17:55:06 +0300

    Update SecurityConfig.java

--- src/main/java/org/gunsugunaydin/RestBase/security/SecurityConfig.java
+++ src/main/java/org/gunsugunaydin/RestBase/security/SecurityConfig.java
@@ -17,14 +17,11 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-
 
 @Configuration
 @EnableWebSecurity
