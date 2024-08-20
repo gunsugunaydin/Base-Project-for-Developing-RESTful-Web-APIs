8eb4f0b7f2a1910872b608f71528110d72fa62c4
Günsu Günaydın <110038957+gunsugunaydin@users.noreply.github.com>
2024-08-20 17:53:09 +0300

    Update AccountRepository.java

--- src/main/java/org/gunsugunaydin/RestBase/repository/AccountRepository.java
+++ src/main/java/org/gunsugunaydin/RestBase/repository/AccountRepository.java
@@ -1,7 +1,6 @@
 package org.gunsugunaydin.RestBase.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.gunsugunaydin.RestBase.model.Account;
 import org.springframework.stereotype.Repository;
